dbd30d9abc3c603bd1a667c083774d64fa1b4c67
경태윤 <xodbsrud320@gmail.com>
2022-02-06 20:23:59 +0900

    sql-gallery

--- rboard, gallery.sql
+++ rboard, gallery.sql
@@ -2,6 +2,9 @@ drop table rboard;
 drop sequence seq_rboard_no;
 drop sequence seq_rgroup_no;
 
+drop table gallery;
+drop sequence seq_gallery_no;
+
 create table rboard(
     no          number,
     user_no     number not null,
@@ -17,12 +20,30 @@ create table rboard(
     references users(no)
 );
 
+create table gallery(
+    no          number,
+    user_no     number,
+    content     varchar2(1000),
+    filePath    varchar2(500),
+    orgName     varchar2(200),
+    saveName    varchar2(500),
+    fileSize    number,
+    primary key(no),
+    constraint gallery_fk foreign key(user_no)
+    references users(no)
+);
+
 create sequence seq_rboard_no
 increment by 1
 start with 1
 nocache;
 
 create sequence seq_rgroup_no
+increment by 1
+start with 1
+nocache;
+
+create sequence seq_gallery_no
 increment by 1
 start with 1
 nocache;
@@ -37,6 +58,10 @@ insert into rboard
 values(seq_rboard_no.nextval, 4, '제목', '내용', 0, sysdate,
     'group_no', 'order_no', 'depth');
 
+insert into gallery
+values(seq_gallery_no.nextval, 'userNo', 'content', 'filePath', 'orgName',
+    'saveName', 'fileSize');
+    
 select  rb.no no,
         title,
         name,
@@ -67,6 +92,25 @@ select  group_no groupNo,
 from rboard
 where no = 1;
 
+select  ga.no,
+        user_no userNo,
+        content,
+        filePath,
+        orgName,
+        saveName,
+        fileSize,
+        name writer
+from gallery ga, users us
+where ga.user_no = us.no
+order by ga.no desc;
+
+select  no,
+        user_no userNo,
+		content,
+        saveName
+from gallery			
+where no = '';
+
 update rboard
 set hit = hit + 1
 where no = 1;
@@ -87,4 +131,8 @@ where no = '';
 
 delete from rboard
 where no = 2;
+
+delete from gallery
+where no = ''
+and user_no = '';
 commit;
